5eeb3b0d7f17152c6e13452d536b3e84901b9ab4
Игорь <101691313+IgorSergeevichISIT@users.noreply.github.com>
2026-04-14 15:21:17 +0700

    Add files via upload

---
+++ assets/create_lab2_docker_server.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -x
+
+if [ "$1" = "" ] ; then
+  echo "Group NAME must be provided"
+  echo "Usage: $0 NAME ID"
+  exit 1
+fi
+if [ "$2" = "" ] ; then
+  echo "Student ID (two digits) must be provided"
+  echo "Usage: $0 NAME ID"
+  exit 1
+fi
+
+group="$1"
+student="$2"
+sshport="${group}${student}22"
+extport1="${group}${student}80"
+extport2="${group}${student}81"
+vpnport="${group}${student}90"
+year="$(date +%y)"
+
+docker run \
+        -d \
+        --name "${group}-student${student}-L2-${year}-server" \
+        --hostname "${group}-student${student}-L2-${year}-server" \
+        --memory="150m" \
+        --memory-swap="150m" \
+        --cpus="0.2" \
+        --pids-limit=300 \
+        -e TZ=Europe/Moscow \
+        -p ${sshport}:22 \
+        -p ${extport1}:${extport1} \
+        -p ${extport2}:${extport2} \
+        -p ${vpnport}:${vpnport} \
+        --privileged vpn_server
+
+# End of file
